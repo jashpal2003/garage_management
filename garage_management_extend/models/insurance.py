cffe8b14a7296854679a5d01bdcc92a2e9ddb05c
jashpal rajpurohit <jashpalpurohit111@gmail.com>
2024-06-20 10:23:15 +0530

    [ADD] Added inherited module. Improved module structure.

---
+++ garage_management_extend/models/insurance.py
@@ -0,0 +1,12 @@
+from odoo import models,fields,api
+
+class Insurance(models.Model):
+    _inherit ='garage.services'
+    _name ='garage.insurance'
+
+
+    accident_description = fields.Char("description of accident")
+    accident_time = fields.Date("Date of accident")
+    insurance = fields.Float("Insurance number")
+    service_ids = fields.Many2many('garage.services', 'garage_ainsurance_id', 'garage_service_id', 'garage_insurance_garage_service_rel', string ="service neede")
+    parts_id = fields.Many2many('customer.parts','garage_binsurance_id','parts_id','insurance_parts_rel', string = "parts needs to recover the vehicle")
